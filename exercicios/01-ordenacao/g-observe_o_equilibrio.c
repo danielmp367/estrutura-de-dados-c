940e527ec1d6893b467cef8adfae8eb9f749dc72
Daniel Pimentel <daniel.m.mendes0@gmail.com>
2023-09-16 00:21:22 -0300

    G lista (ordenação)

---
+++ exercicios/01-ordenacao/g-observe_o_equilibrio.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static void heapify(int *v, size_t i, size_t size) {
+  int left;
+  int right;
+  int largest;
+  while (i < size) {
+    left = (i * 2) + 1;
+    right = (i * 2) + 2;
+    largest = i;
+
+    if (left < size && v[left] > v[largest]) {
+      largest = left;
+    }
+
+    if (right < size && v[right] > v[largest]) {
+      largest = right;
+    }
+
+    if (i == largest) {
+      break;
+    }
+
+    int tmp = v[i];
+    v[i] = v[largest];
+    v[largest] = tmp;
+    i = largest;
+  }
+}
+
+static void make_heap(int *v, size_t size) {
+  int i;
+  for (i = size / 2; i >= 0; i--) {
+    heapify(v, i, size);
+  }
+}
+
+void heap_sort(int *v, size_t size) {
+  int i;
+  make_heap(v, size);
+  for (i = size - 1; i > 0; i--) {
+    int tmp = v[i];
+    v[i] = v[0];
+    v[0] = tmp;
+    heapify(v, 0, i);
+  }
+}
+
+void print_vetor(int *v, int n) {
+  for (size_t i = 0; i < n; i++) {
+    printf("%d\n", v[i]);
+  }
+}
+
+int sequencia_equilibrada(int *v, int n) {
+  int equilibrada = 1; // assume verdadeiro
+  for (size_t i = 0, j = n - 1; i < n && j >= 0; i++, j--) {
+    if ((v[i] != 0) && (v[j] % v[i] != 0)) {
+      equilibrada = 0;
+      break;
+    }
+  }
+  return equilibrada;
+}
+
+int main() {
+  int n;
+  scanf("%d", &n);
+
+  int *v = malloc(sizeof(int) * n);
+
+  for (size_t i = 0; i < n; i++) {
+    scanf("%d", &v[i]);
+  }
+
+  heap_sort(v, n);
+
+  int equilibrada = sequencia_equilibrada(v, n);
+  printf("%s\n", equilibrada ? "Sim" : "Nao");
+
+  free(v);
+
+  return 0;
+}
