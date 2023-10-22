a5d3f361d7eff0f788e89928bcdee65d3bc844ea
Daniel Pimentel <daniel.m.pimentel@hotmail.com>
2023-10-22 20:38:34 -0300

    lista (pilhas) exercicio C (falha 13)

---
+++ exercicios/04-pilha/c-apagando_e_ganhando.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct {
+  char *items;
+  size_t capacity;
+  size_t length;
+} stack_t;
+
+void stack_init(stack_t **s) {
+  (*s) = malloc(sizeof(stack_t));
+  (*s)->capacity = 16;
+  (*s)->length = 0;
+  (*s)->items = malloc(sizeof(char) * (*s)->capacity);
+}
+
+void stack_push(stack_t *s, char c) {
+  if (s->length == s->capacity) {
+    s->capacity *= 2;
+    s->items = realloc(s->items, sizeof(char) * s->capacity);
+  }
+
+  s->items[s->length++] = c;
+}
+
+int stack_empty(stack_t *s) { return (s->length == 0); }
+
+void stack_pop(stack_t *s) {
+  if (s->length < (s->capacity / 4)) {
+    s->capacity /= 2;
+    s->items = realloc(s->items, sizeof(char) * s->capacity);
+  }
+  if (!stack_empty(s)) {
+    s->length--;
+  }
+}
+
+char stack_top(stack_t *s) { return s->items[s->length - 1]; }
+
+void stack_delete(stack_t **s) {
+  free((*s)->items);
+  free(*s);
+  *s = NULL;
+}
+
+
+void remove_digits(char *seq, int n, int d) {
+  int qtd_items = d;
+  stack_t *s;
+  stack_init(&s);
+  for (size_t i = 0; i < n; i++) {
+    char c = seq[i];
+    while (!stack_empty(s) && qtd_items > 0 && c > stack_top(s)) {
+      stack_pop(s);
+      qtd_items --;
+    }
+    stack_push(s, c);
+  }
+  s->length = n - d;
+
+  for (size_t i = 0; i < s->length; i++) {
+    printf("%c", s->items[i]);
+  }
+  printf("\n");
+
+  stack_delete(&s);
+  
+}
+
+
+int main() {
+
+  char *buffer;
+  int n = 0;
+  int d = 0;
+
+  while (1) {
+    scanf("%d %d", &n, &d);
+    if(n == 0 || d == 0) {
+      break;
+    }
+    buffer = malloc(sizeof(char) * n);
+    scanf("%s", buffer);
+    // buf_clean();
+    remove_digits(buffer, n, d);
+  
+    free(buffer);
+  }
+
+ 
+
+  return 0;
+}
